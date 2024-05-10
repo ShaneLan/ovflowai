d4a340a93c9618a3ec46074317814242dcfb69f9
shane <lan.shine.me@gmail.com>
2024-05-10 20:54:45 +0800

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,15 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
+// import { getAnalytics } from "firebase/analytics";
+// const analytics = getAnalytics(app);
+
 // Firebase Config
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCf_Sw4OByvrXdmVpyT9rlfI7AqFGDH-60",
+  authDomain: "ovflowai.firebaseapp.com",
+  projectId: "ovflowai",
+  storageBucket: "ovflowai.appspot.com",
+  messagingSenderId: "362208467929",
+  appId: "1:362208467929:web:027a7e6bb64e709c9457c8",
+  measurementId: "G-VRH9M24953"
 };
 
 const app = initializeApp(firebaseConfig);
